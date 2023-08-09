14fbba6cb10e30b770d07ab1ccfe04155741e811
Abhinav Singh <113774414+codesabhinav@users.noreply.github.com>
2023-08-09 23:22:26 +0530

    Add files via upload

---
+++ snakenew.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Snake Game</title>
+	<link rel="stylesheet" type="text/css" href="snakenew.css">
+	<script src="snakenew.js"></script>
+</head>
+<body>
+	<h1>Snake Game using Javascript</h1>
+	<canvas id="board"></canvas>
+</body>
+</html>
